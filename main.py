bf40d43495ff5d091ff423e275a84e1abc5c7650
germangamboa95 <germangamboa95@gmail.com>
2023-03-28 17:23:12 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+
+import random
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+facts: list[str]
+
+with open("./facts.txt", "r") as f:
+    facts = f.readlines()
+
+
+@app.get("/")
+def read_root():
+    '''
+    Free random fact about snakes
+    '''
+    return {"fact": facts[random.randint(0, len(facts) - 1)]}
