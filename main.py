db19fa48ae460fb52212a946b848d356041c7b86
germangamboa95 <germangamboa95@gmail.com>
2023-03-28 18:04:31 -0400

    make it more like an api

--- main.py
+++ main.py
@@ -11,9 +11,18 @@ with open("./facts.txt", "r") as f:
     facts = f.readlines()
 
 
-@app.get("/")
-def read_root():
+@app.get("/:id")
+def read_root(id: int):
+    '''
+    Fact about snakes
+    '''
+    return {id: id, "fact": facts[id + 1].strip()}
+
+
+@app.get("/random")
+def get_random():
     '''
     Free random fact about snakes
     '''
-    return {"fact": facts[random.randint(0, len(facts) - 1)].strip()}
+    id = random.randint(0, len(facts) - 1)
+    return {id: id, "fact": facts[id].strip()}
