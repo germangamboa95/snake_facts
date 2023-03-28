e607d2da51d5e134aeeb72af4fa0b2cd7c1323d5
germangamboa95 <germangamboa95@gmail.com>
2023-03-28 18:31:41 -0400

    docs

--- main.py
+++ main.py
@@ -12,20 +12,20 @@ with open("./facts.txt", "r") as f:
     facts = f.readlines()
 
 
-@app.get("/{id}")
-def get_fact_by_id(id: int):
-    '''
-    Fact about snakes
-    '''
-    return {"id": id, "fact": facts[id].strip()}
-
-
 @app.get("/random")
 def get_random_fact():
     '''
     Free random fact about snakes
     '''
     id = random.randint(0, len(facts) - 1)
+    return {"id": id, "fact": facts[id].strip()}
+
+
+@app.get("/{id}")
+def get_fact_by_id(id: int):
+    '''
+    Fact about snakes
+    '''
     return {"id": id, "fact": facts[id].strip()}
 
 
